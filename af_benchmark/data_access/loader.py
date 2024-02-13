546bf5611c72273a1f0128d0f2406df82b2d2ec0
Dmitry <kondratyev.d.95@gmail.com>
2024-02-13 19:33:28 +0100

    load files from local dir

--- af_benchmark/data_access/loader.py
+++ af_benchmark/data_access/loader.py
@@ -1,10 +1,15 @@
+import glob
+
 def get_file_list(cls):
     mode = cls.config.get('data-access.mode', 'local')
     if mode == 'local':
         file_list = cls.config.get('data-access.files', [])
+    elif mode == 'local_dir':
+        files_dir = cls.config.get('data-access.files_dir', "")
+        file_list = glob.glob(files_dir+"/*.root")
     else:
         raise NotImplementedError(
-            f"Data access modes other than 'local' are not yet implemented"
+            f"Data access modes other than 'local' and 'local_dir' are not yet implemented"
         )
         
     return file_list
